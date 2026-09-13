448816026d87686ea82f0aaa57f0d132f98d0a78
saumitrapal <saumitra31415926@gmail.com>
2026-09-13 12:12:38 +0530

    add some file

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -5,18 +5,19 @@ from quiz_brain import QuziBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     
     new_question = Question(question_text, question_answer)
     
     question_bank.append(new_question)
     
-
+# print(len(question_bank))
 
 quiz = QuziBrain(question_bank)
 
 while quiz.still_has_question:
     quiz.next_question()
     
-print("You complete quiz")
+print("You completed quiz")
+print(f"Your Final Score was: {quiz.score}/{quiz.question_number}")
--- quiz-game-start/quiz_brain.py
+++ quiz-game-start/quiz_brain.py
@@ -4,7 +4,6 @@ class QuziBrain:
         self.question_list = q_list
         self.score = 0
         
-        
     def still_has_question(self):
         if self.question_number < len(self.question_list):
             return True
@@ -15,7 +14,7 @@ class QuziBrain:
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
-        self.check_answer(user_answer, current_question.answer) 
+        self.check_answer(user_answer, current_question.answer)
                 
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
